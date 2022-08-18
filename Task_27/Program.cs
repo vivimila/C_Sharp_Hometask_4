122a343fecc3eabd5361bcfa2b2afe2488116761
vivimila <violamilashevskaya@yandex.by>
2022-08-18 16:40:02 +0300

    Task 27 is solved

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -4,4 +4,15 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
+int sum = 0;
+for (int i = 0; i<= num; i++)
+{
+    int digit = num % 10;
+    sum = sum + digit;
+    num =  num / 10;
+}
+
+Console.WriteLine($"Сумма цифр из введенного числа  = {sum}");
