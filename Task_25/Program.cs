fd4e96c270dd530c72a669dca9e8a7f6a9af049b
vivimila <violamilashevskaya@yandex.by>
2022-08-19 14:29:54 +0300

    Task 25 is solved

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -3,3 +3,15 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int grade = A;
+for (int i = 1; i< B; i++) // почему i  не может начинаться с 0?
+{
+    grade = grade * A;
+}
+
+Console.WriteLine($"Число {A} в степени {B}  = {grade}");
