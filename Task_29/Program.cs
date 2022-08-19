44348c9461a9e6d37f0eadc5490716b9fab929d5
vivimila <violamilashevskaya@yandex.by>
2022-08-19 15:00:26 +0300

    Work with a folder task 29

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Write("Введите число элементов массива: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 0)
+{
+Console.Write("Вы ввели недопустимое значение");
+return;
+}
+int[] GetArray(int size)
+{
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+{
+array[i] = new Random().Next(0, 2);
+}
+return array;
+}
+int[] result = GetArray(number);
+
+// void PrintArray(int[] array)
+// {
+// Console.Write("[");
+// for (int i = 0; i < array.Length - 1; i++)
+// {
+// Console.Write($"{array[i]},");
+// }
+// Console.Write($"{array[array.Length - 1]}]");
+// }
+
+// PrintArray(result);
